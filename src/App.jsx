90d3405dcc14db933624023564256c31110b8192
Firdavs Temirov <94130544+firdavs13@users.noreply.github.com>
2022-02-25 21:10:53 +0500

    initiak commit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import "./App.css";
+import { useState, useRef } from "react";
+import data from "./localization/content";
+import uzFlag from "./assets/images/uz.svg";
+
+function App() {
+  const inputValue = useRef();
+  const [theme, settheme] = useState("light");
+
+  function onChangeFunction() {
+    if (inputValue.current.checked) {
+      settheme("dark");
+    } else {
+      settheme("light");
+    }
+  }
+
+  const [lang, setLang] = useState("eng");
+
+  const [openModal, setOpenModal] = useState(false);
+
+  return (
+    <div data-theme={theme} className="App">
+      <section className="box__wrapper">
+        <button
+          onClick={() => setOpenModal(!openModal)}
+          className="box__btn modal__btn"
+        >
+          {" "}
+          Modal{" "}
+        </button>
+        {openModal && (
+          <div class="container">
+            <h1 className="box__heading">{data[lang].heading}</h1>
+            <p className="box__desc">{data[lang].desc}</p>
+            <button onClick={() => setOpenModal(false)} className="box__btn">
+              {data[lang].button}
+            </button>
+          </div>
+        )}
+
+        <select
+          className="box__select"
+          defaultValue={lang}
+          onChange={(e) => setLang(e.target.value)}
+        >
+          <option value="ru">Ru</option>
+          <option value="uz"> Uz</option>
+          <option value="eng">Eng</option>
+        </select>
+      </section>
+
+      <div className="theme-switcher">
+        <input
+          className="input"
+          ref={inputValue}
+          onChange={() => onChangeFunction()}
+          type="checkbox"
+          id="switcher"
+        />
+        <label className="label" for="switcher">
+          Switch
+        </label>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/localization/content.jsx
@@ -0,0 +1,19 @@
+const data = {
+  ru: {
+    heading:"Портативный монитор Desklab: идеально подходит для работы, игр и мобильных устройств",
+    desc:"15,6-дюймовый портативный сенсорный монитор Desklab IPS 4K — хороший выбор, если вы ищете высокопроизводительный второй дисплей, который можно легко взять с собой. Это не новый продукт. Скорее, это последняя версия конфигурации монитора, основанная на инновационной концепции, выпущенной около двух лет назад. Монитор Desklab не просто превращает сотовый телефон в игровой дисплей с большим экраном или временную рабочую станцию, когда компьютер недоступен. Его полезность выходит далеко за рамки этого сценария. Он работает с ПК под управлением Microsoft Windows, macOS, Linux и Chromebook (Chrome OS). Более того, он совместим с игровыми консолями, а также с устройствами Android и Apple и планшетами.",
+    button: "Близко",
+  },
+  uz: {
+    heading:"Desklab portativ monitor: Ish, o'yin, mobil mahsuldorlik uchun ideal",
+    desc:"15,6 dyuymli Desklab portativ IPS 4K sensorli displey monitori, agar siz o‘zingiz bilan osongina olib ketishingiz mumkin bo‘lgan yuqori samarali ikkinchi displeyni izlayotgan bo‘lsangiz, ko‘rib chiqish uchun yaxshi tanlovdir. Bu mutlaqo yangi mahsulot emas. Aksincha, bu taxminan ikki yil oldin chiqarilgan innovatsion kontseptsiyaga asoslangan monitor konfiguratsiyasining so'nggi iteratsiyasi. Desklab monitori mobil telefonni katta ekranli o'yin displeyi yoki kompyuter mavjud bo'lmaganda vaqtinchalik ish stantsiyasiga aylantirishdan ko'proq narsani qiladi. Uning foydaliligi bu stsenariydan ancha yuqori. U Microsoft Windows, macOS, Linux va Chromebook (Chrome OS) bilan ishlaydigan shaxsiy kompyuterlarda ishlaydi. Bundan ham yaxshiroq, u o'yin pristavkalari, shuningdek, Android va Apple qurilmalari va planshetlar bilan mos keladi.",
+    button: "Yopish",
+  },
+  eng: {
+    heading:" Desklab Portable Monitor: Ideal for Work, Play, Mobile Productivity",
+    desc:"The 15.6″ Desklab portable IPS 4K touchscreen monitor is a solid choice to consider if you are looking for a high-performance second display that you can easily take with you. This is not a brand new product. Rather, it is the latest iteration of monitor configurations based on an innovative concept released about two years ago. The Desklab monitor does more than turn a cell phone into a large-screen gaming display or temporary workstation when no computer is available. Its usefulness goes well beyond thatscenario. It works from PCs running Microsoft Windows, macOS, Linux, and Chromebooks (Chrome OS). Even better, it is compatible with gaming consoles, as well as Android and Apple devices, and tablets.",
+    button: "Close",
+  },
+};
+
+export default data;
